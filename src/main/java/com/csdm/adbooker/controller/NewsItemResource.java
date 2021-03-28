5f8a530a525857c533c92e1b8d2f3e8de98f6900
yakup.kariksiz <yakup.kariksiz@epias.com.tr>
2021-03-28 23:08:30 +0300

    feat: graphql integration was completed

--- src/main/java/com/csdm/adbooker/controller/NewsItemResource.java
+++ src/main/java/com/csdm/adbooker/controller/NewsItemResource.java
@@ -1,5 +1,10 @@
 package com.csdm.adbooker.controller;
 
+import com.csdm.adbooker.service.GraphQLService;
+import graphql.ExecutionResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NewsItemResource {
 
-    @PostMapping
-    public void getAllNewsItems(@RequestBody String query) {
+    @Autowired
+    private GraphQLService graphQLService;
 
+    @PostMapping
+    public ResponseEntity<Object> getAllNewsItems(@RequestBody String query) {
+        ExecutionResult execute = graphQLService.getGraphQLService().execute(query);
+
+        return new ResponseEntity<>(execute, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/csdm/adbooker/service/GraphQLService.java
+++ src/main/java/com/csdm/adbooker/service/GraphQLService.java
@@ -1,13 +1,56 @@
 package com.csdm.adbooker.service;
 
+import com.csdm.adbooker.service.datafetcher.AllNewsItemsDataFetcher;
+import com.csdm.adbooker.service.datafetcher.NewsItemDataFetcher;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+
 @Service
 public class GraphQLService {
 
+    private final AllNewsItemsDataFetcher allNewsItemsDataFetcher;
+    private final NewsItemDataFetcher newsItemDataFetcher;
+    
     @Value("classpath:news.graphql")
     Resource resource;
 
+    private GraphQL graphQLService;
+
+    public GraphQLService(AllNewsItemsDataFetcher allNewsItemsDataFetcher, NewsItemDataFetcher newsItemDataFetcher) {
+        this.allNewsItemsDataFetcher = allNewsItemsDataFetcher;
+        this.newsItemDataFetcher = newsItemDataFetcher;
+    }
+
+    @PostConstruct
+    private void loadSchema() throws IOException {
+        File schemaFile = resource.getFile();
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(schemaFile);
+        RuntimeWiring wiring = buildRuntimeWiring();
+        GraphQLSchema schema = new SchemaGenerator().makeExecutableSchema(typeRegistry, wiring);
+        graphQLService = GraphQL.newGraphQL(schema).build();
+    }
+
+    private RuntimeWiring buildRuntimeWiring() {
+        return RuntimeWiring
+                .newRuntimeWiring()
+                .type("Query", typeWiring -> typeWiring
+                        .dataFetcher("allNewsItems", allNewsItemsDataFetcher)
+                        .dataFetcher("newsItem", newsItemDataFetcher))
+                .build();
+    }
+
+    public GraphQL getGraphQLService() {
+        return graphQLService;
+    }
 }
