fc080c4cd2561d7f6c9d72e574481f710e20de84
yakup.kariksiz <yakup.kariksiz@epias.com.tr>
2021-03-29 15:24:51 +0300

    fix: add index and correct rss feed interval time as 5 minutes.

--- src/main/java/com/csdm/adbooker/model/NewsItemParameters.java
+++ src/main/java/com/csdm/adbooker/model/NewsItemParameters.java
@@ -4,6 +4,6 @@ public interface NewsItemParameters {
 
     static final String NEWS_FEED_URL = "http://feeds.nos.nl/nosjournaal?format=xml";
     static final int RSS_FEED_SIZE_THRESHOLD = 10;
-    static final int RSS_FEED_INTERVAL_TIME = 5000;
+    static final int RSS_FEED_INTERVAL_TIME = 5000 * 60;
     
 }
